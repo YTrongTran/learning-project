8ec09d8b41303c184b33ac406c9dfe4844344b70
dat.edric <trandatyl1@gmail.com>
2024-12-10 21:29:11 +0700

    setup layout

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<p class="text-center">
+    © Copyright 2022 ILA. All rights reserved.
+Công ty TNHH ILA Việt Nam, GCN đăng ký đầu tư số 411043000734 ngày cấp 08/09/2017 nơi cấp Sở Kế Hoạch & Đầu Tư TP.HCM.
+Trụ sở chính: The Crest Residence, Khu đô thị Metropole Thủ Thiêm, 15 Trần Bạch Đằng, Phường Thủ Thiêm, Thành phố Thủ Đức, Thành Phố Hồ Chí Minh.
+</p>
