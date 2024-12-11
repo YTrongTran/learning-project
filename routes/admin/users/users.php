9edbec07ca8e7823dd7cc9961b69c983c4e0249c
YTrongTran <trantrongycntt@gmail.com>
2024-12-11 14:19:23 +0700

    update folder router init project

---
+++ routes/admin/users/users.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->group(function(){
+    Route::get('/users',function(){
+        return 'đây là admin users';
+    });
+});
+
+
+
+
+?>
