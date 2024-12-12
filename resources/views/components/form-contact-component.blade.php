8cc4827827b841d59400a7e4bb66aaaf1e540446
Hoang-149 <thanghoangminh149@gmail.com>
2024-12-12 21:40:11 +0700

    blog page

--- resources/views/components/form-contact-component.blade.php
+++ resources/views/components/form-contact-component.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <form action="">form contact</form>
+<div class="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
+    <form action="">
+        <div class="mb-5">
+          <label for="name" class="block mb-2 font-bold text-gray-600">Name</label>
+          <input type="text" id="name" name="name" placeholder="Put in your fullname." class="border border-gray-300 shadow p-3 w-full rounded mb-">
+        </div>
+
+        <div class="mb-5">
+          <label for="name" class="block mb-2 font-bold text-gray-600">Name</label>
+          <input type="text" id="name" name="name" placeholder="Put in your fullname." class="border border-gray-300 shadow p-3 w-full rounded mb-">
+        </div>
+
+        <div class="mb-5">
+          <label for="email" class="block mb-2 font-bold text-gray-600">Name</label>
+          <input type="email" id="email" name="email" placeholder="Put in your fullname." class="border border-gray-300 shadow p-3 w-full rounded mb-">
+        </div>
+
+        <button class="block w-full bg-blue-500 text-white font-bold p-4 rounded-lg">Submit</button>
+      </form>
 </div>
