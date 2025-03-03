0e9f4f7b57c9968f52a473a8ad6b511f19cdaab1
Hoang-149 <thanghoangminh149@gmail.com>
2025-03-04 01:16:50 +0700

    update railway

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,27 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    base: '/',
-    plugins: [laravel(['resources/js/app.js'])],
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            protocol: 'wss', // Fix WebSocket Secure khi dùng HTTPS
-            host: process.env.RAILWAY_STATIC_URL
-        }
-    },
-    build: {
-        outDir: 'public/build',
-        assetsDir: 'assets'
-    }
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
 });
 
-
+// export default defineConfig({
+//     base: '/',
+//     plugins: [laravel(['resources/js/app.js'])],
+//     server: {
+//         host: '0.0.0.0',
+//         hmr: {
+//             protocol: 'wss', // Fix WebSocket Secure khi dùng HTTPS
+//             host: process.env.RAILWAY_STATIC_URL
+//         }
+//     },
+//     build: {
+//         outDir: 'public/build',
+//         assetsDir: 'assets'
+//     }
+// });
