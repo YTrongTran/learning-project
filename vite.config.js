7a8ec7b748ab89905036bcc848a59c70366fb9d8
Hoàng Minh Thắng <84968004+Hoang-149@users.noreply.github.com>
2025-03-04 01:01:40 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+// });
+
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+    plugins: [laravel(['resources/js/app.js'])],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: process.env.RAILWAY_STATIC_URL, // Fix HMR khi deploy
+        }
+    },
+    build: {
+        outDir: 'public/build', // Đảm bảo build vào thư mục public/build
+    }
 });
+
