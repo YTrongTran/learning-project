3fa4024ca1e4342ad1a499321a754bc78e2348f4
YTrongTran <trantrongycntt@gmail.com>
2025-03-04 16:43:38 +0700

    update config

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -7,7 +7,8 @@
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.bunny.net" />
 <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-<link rel="icon" href="/assets/img/logoheader.png">
+<link rel="icon" type="image/png" href="{{ secure_asset('assets/img/logoheader.png') }}">
+
 
 <link rel="stylesheet" type="text/css"
     href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" />
